a30a86b121e0c86f961d9758c9ea072e3bda42af
parth <parthematics@gmail.com>
2019-07-25 01:38:34 -0700

    added google analytics

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,3 @@
-// /* GOOGLE ANALYTICS */
-// (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-// (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-// m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-// })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-//
-// ga('create', 'UA-70469459-6', 'auto');
-// ga('send', 'pageview');
-//
-// function gaEvent(clickedItem){
-//   ga('send', 'event', clickedItem, 'click');
-// }
-
-
 
 /* INITIALIZATION */
 window.onload = function() { document.body.className = ''; }
